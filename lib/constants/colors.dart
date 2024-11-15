c6ed5186add5117c5f95e48b8db98109c42413d7
Nguyen Tan <vantankmt@gmail.com>
2024-11-15 16:37:29 +0700

    feat: ui search and carousel slider

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color orange = Color(0xFFF83758);
   static const Color grey = Color(0xFF9E9E9E);
+  static const Color grey2 = Color(0xFF5F5D5D);
+  static const Color grey3 = Color(0xFF9B9999);
   static const Color main = Color(0xFF6055D8);
   static const Color main2 = Color(0xFFF8F7F7);
 }
--- lib/screen/ecommer/home/home_screen.dart
+++ lib/screen/ecommer/home/home_screen.dart
@@ -1,6 +1,21 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_new_architectua/constants/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+          child: Image.network(item, fit: BoxFit.cover),
+        ))
+    .toList();
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,48 +25,139 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController controllerSearch = TextEditingController();
+
+  int _current = 0;
+  final CarouselSliderController _controllerAds = CarouselSliderController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 25,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Row(
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.only(right: 10),
-              child: CircleAvatar(
-                radius: 26,
-                backgroundImage:
-                    NetworkImage('https://picsum.photos/id/237/200/300'),
+      body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Container(
+              decoration: const BoxDecoration(),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: CircleAvatar(
+                          radius: 26,
+                          backgroundImage: NetworkImage(
+                              'https://picsum.photos/id/237/200/300'),
+                        ),
+                      ),
+                      const Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Hello!', style: TextStyle(fontSize: 12)),
+                          Text('John William',
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                      const Expanded(child: SizedBox()),
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.main2,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(13, 10, 10, 10),
+                          child: SvgPicture.asset('assets/icons/bell_icon.svg'),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: AppColors.main2,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 0, horizontal: 14),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: SvgPicture.asset(
+                                'assets/icons/search_icon.svg',
+                                colorFilter: const ColorFilter.mode(
+                                    AppColors.grey2, BlendMode.srcIn),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: TextField(
+                              controller: controllerSearch,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Search here',
+                                hintStyle: TextStyle(
+                                    color: AppColors.grey3, fontSize: 14),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  CarouselSlider(
+                    items: imageSliders,
+                    carouselController: _controllerAds,
+                    options: CarouselOptions(
+                        autoPlay: true,
+                        viewportFraction: 1,
+                        enlargeCenterPage: true,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _current = index;
+                          });
+                        }),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: imgList.asMap().entries.map((entry) {
+                      return GestureDetector(
+                        onTap: () => _controllerAds.animateToPage(entry.key),
+                        child: Container(
+                          width: 12.0,
+                          height: 12.0,
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 4.0),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: (Theme.of(context).brightness ==
+                                          Brightness.dark
+                                      ? Colors.white
+                                      : Colors.black)
+                                  .withOpacity(
+                                      _current == entry.key ? 0.9 : 0.4)),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ],
               ),
             ),
-            const Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Hello!', style: TextStyle(fontSize: 12)),
-                Text('John William', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
-              ],
-            ),
-            const Expanded(child: SizedBox()),
-            Container(
-              width: 40,
-              height: 40,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: AppColors.main2,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(13, 10, 10, 10),
-                child: SvgPicture.asset('assets/icons/bell_icon.svg'),
-              ),
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
