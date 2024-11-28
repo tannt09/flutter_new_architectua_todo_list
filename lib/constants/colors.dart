085c9b1e3c96ab2e9d60772ae0fd7b0d3a182afe
Nguyen Tan <vantankmt@gmail.com>
2024-11-28 12:14:03 +0700

    feat: ui for search screen

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,6 +5,7 @@ class AppColors {
   static const Color grey = Color(0xFF9E9E9E);
   static const Color grey2 = Color(0xFF5F5D5D);
   static const Color grey3 = Color(0xFF9B9999);
+  static const Color grey4 = Color(0xFF817F7F);
   static const Color main = Color(0xFF6055D8);
   static const Color main2 = Color(0xFFF8F7F7);
 }
--- lib/screen/ecommer/search/search_screen.dart
+++ lib/screen/ecommer/search/search_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_new_architectua/constants/colors.dart';
 import 'package:flutter_new_architectua/core/bloc/goods/goods_bloc.dart';
+import 'package:flutter_new_architectua/main.dart';
+import 'package:flutter_new_architectua/model/goods_model.dart';
+import 'package:flutter_new_architectua/widget/home/product_item_widget.dart';
 import 'package:flutter_new_architectua/widget/search_widget.dart';
-import 'package:flutter_svg/svg.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -15,17 +18,17 @@ class _SearchPageState extends State<SearchPage> {
   TextEditingController controllerSearch = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+    blocGoods.add(const GetAllGoodsEvent());
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Padding(
-            padding: EdgeInsets.only(right: 20),
-            child: SearchWidget(controller: controllerSearch)),
-        leading: IconButton(
-          icon: SvgPicture.asset('assets/icons/left_arrow_icon.svg'),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        titleSpacing: 0,
+        toolbarHeight: 70,
+        title: SearchWidget(controller: controllerSearch),
       ),
       body: GestureDetector(
         onTap: () {
@@ -33,21 +36,73 @@ class _SearchPageState extends State<SearchPage> {
         },
         child: BlocBuilder<GoodsBloc, GoodsState>(
           builder: (context, state) {
-            // if (!state.goods.isNotEmpty) {
-            //   return const Center(
-            //     child: Text("Is empty"),
-            //   );
-            // }
+            Future<void> changeFavoriteState(GoodsModel item) async {
+              final id = item.productId;
+              if (id != null) {
+                blocGoods.add(ChangeFavoriteStateEvent(item: item));
+                blocGoods.add(const GetAllGoodsEvent());
+              }
+            }
 
             return SingleChildScrollView(
               child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                padding: const EdgeInsets.symmetric(vertical: 14),
                 child: Container(
                   decoration: const BoxDecoration(),
                   child: Column(
                     children: <Widget>[
-                      // SearchWidget(controller: controllerSearch),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text.rich(
+                              TextSpan(
+                                text: 'Results for ',
+                                style: TextStyle(
+                                    color: AppColors.grey4,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: '“ Shoes “',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const Text(
+                              "6 Results Found",
+                              style: TextStyle(
+                                color: AppColors.main,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 15,
+                          childAspectRatio: 1,
+                        ),
+                        itemCount: state.goods.length,
+                        itemBuilder: (context, index) {
+                          final item = state.goods[index];
+                          return ItemProductWidget(
+                              goods: item,
+                              changeFavoriteState: changeFavoriteState);
+                        },
+                      )
                     ],
                   ),
                 ),
