ed7ddbaf41d326a6df5778cdbe458214c373c811
Nguyen Tan <vantankmt@gmail.com>
2024-12-26 16:00:25 +0700

    feat: handle when image button pressed

--- lib/widget/profile/edit_avatar_widget.dart
+++ lib/widget/profile/edit_avatar_widget.dart
@@ -1,9 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:image_picker/image_picker.dart';
 
-class EditAvatarWidget extends StatelessWidget {
+class EditAvatarWidget extends StatefulWidget {
   final String avatarUrl;
   const EditAvatarWidget({super.key, required this.avatarUrl});
+
+  @override
+  State<EditAvatarWidget> createState() => _EditAvatarWidgetState();
+}
+
+class _EditAvatarWidgetState extends State<EditAvatarWidget> {
+  List<XFile>? _mediaFileList;
+  dynamic _pickImageError;
+
+  final ImagePicker _picker = ImagePicker();
+
+  final TextEditingController maxWidthController = TextEditingController();
+  final TextEditingController maxHeightController = TextEditingController();
+  final TextEditingController qualityController = TextEditingController();
+  final TextEditingController limitController = TextEditingController();
+
+  void _setImageFileListFromFile(XFile? value) {
+    _mediaFileList = value == null ? null : <XFile>[value];
+  }
+
+  Future<void> _onImageButtonPressed(
+    ImageSource source, {
+    required BuildContext context,
+    bool isMultiImage = false,
+    bool isMedia = false,
+  }) async {
+    if (context.mounted) {
+      await _displayPickImageDialog(context, false, (double? maxWidth,
+          double? maxHeight, int? quality, int? limit) async {
+        try {
+          final XFile? pickedFile = await _picker.pickImage(
+              source: source,
+              maxWidth: maxWidth,
+              maxHeight: maxHeight,
+              imageQuality: quality);
+          setState(() {
+            _setImageFileListFromFile(pickedFile);
+          });
+        } catch (e) {
+          setState(() {
+            _pickImageError = e;
+          });
+        }
+      });
+    }
+  }
+
+  Future<void> _displayPickImageDialog(
+      BuildContext context, bool isMulti, OnPickImageCallback onPick) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add optional parameters'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: maxWidthController,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  decoration: const InputDecoration(
+                      hintText: 'Enter maxWidth if desired'),
+                ),
+                TextField(
+                  controller: maxHeightController,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  decoration: const InputDecoration(
+                      hintText: 'Enter maxHeight if desired'),
+                ),
+                TextField(
+                  controller: qualityController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Enter quality if desired'),
+                ),
+                if (isMulti)
+                  TextField(
+                    controller: limitController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        hintText: 'Enter limit if desired'),
+                  ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("CANCEL")),
+              TextButton(
+                  onPressed: () {
+                    final double? width = maxWidthController.text.isNotEmpty
+                        ? double.parse(maxWidthController.text)
+                        : null;
+                    final double? height = maxHeightController.text.isNotEmpty
+                        ? double.parse(maxHeightController.text)
+                        : null;
+                    final int? quality = qualityController.text.isNotEmpty
+                        ? int.parse(qualityController.text)
+                        : null;
+                    final int? limit = limitController.text.isNotEmpty
+                        ? int.parse(limitController.text)
+                        : null;
+                    onPick(width, height, quality, limit);
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('PICK'))
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +129,7 @@ class EditAvatarWidget extends StatelessWidget {
         children: [
           CircleAvatar(
             radius: 60,
-            backgroundImage: NetworkImage(avatarUrl),
+            backgroundImage: NetworkImage(widget.avatarUrl),
           ),
           Positioned(
             right: 4,
@@ -25,4 +140,15 @@ class EditAvatarWidget extends StatelessWidget {
       ),
     );
   }
+
+  @override
+  void dispose() {
+    maxWidthController.dispose();
+    maxHeightController.dispose();
+    qualityController.dispose();
+    super.dispose();
+  }
 }
+
+typedef OnPickImageCallback = void Function(
+    double? maxWidth, double? maxHeight, int? quality, int? limit);
